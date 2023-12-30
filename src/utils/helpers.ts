e20d0b3d2d84f14d57a205c727f9b85df5dc1904
Omoshlawi <lawiomosh3@gmail.com>
2023-12-30 14:38:44 +0300

    Added extendend version of helper util object to form data that also handle object with data of type file

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import { Express } from "express";
+import { has } from "lodash";
 
 export const expressMulterFileToFile = (
   expressMulterFie: Express.Multer.File
@@ -35,6 +36,50 @@ export const objectToFormData = (
         } else if (typeof value === "object" && !(value instanceof File)) {
           // Recursively handle nested objects
           objectToFormData(value, formData, formKey);
+        } else {
+          formData.append(formKey, value as string);
+        }
+      }
+    }
+  }
+  return formData;
+};
+
+// Suport object with files objects too
+export const _objectToFormData = (
+  data: { [key: string]: any },
+  formData: FormData = new FormData(),
+  parentKey?: string
+): FormData => {
+  for (const key in data) {
+    if (has(data, key)) {
+      const value = data[key];
+
+      const formKey = parentKey ? `${parentKey}[${key}]` : key;
+
+      if (value !== null && value !== undefined) {
+        if (value instanceof Array) {
+          value.forEach((val, index) => {
+            const nestedFormKey = `${formKey}[${index}]`;
+
+            if (typeof val === "object" && !(val instanceof File)) {
+              // Recursively handle nested objects in arrays
+              objectToFormData(val, formData, nestedFormKey);
+            } else {
+              // Handle File instances in arrays
+              if (val instanceof File) {
+                formData.append(nestedFormKey, val, val.name);
+              } else {
+                formData.append(nestedFormKey, val);
+              }
+            }
+          });
+        } else if (typeof value === "object" && !(value instanceof File)) {
+          // Recursively handle nested objects
+          objectToFormData(value, formData, formKey);
+        } else if (value instanceof File) {
+          // Handle top-level File instances
+          formData.append(formKey, value, value.name);
         } else {
           formData.append(formKey, value as string);
         }
